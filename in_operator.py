615b16af3eea4f6018799782ce8dd01c0bed5eb5
ahmadfaraz2 <ahmadfarazjanjua780@gmail.com>
2024-01-31 21:56:11 +0500

    Learn about in and not operators in Python

---
+++ in_operator.py
@@ -0,0 +1,19 @@
+# my_answer = input("What is your answer: ")
+# options = ["rock", "scissors", "paper"]
+
+# if my_answer in options:
+#     print("That option is a viable option")
+# else: 
+#     print("Wrong answer try again!") 
+
+
+key = "name"
+person = {
+    "name" : "Ahmad Faraz",
+    "profession" : "Software Engineer"
+}
+
+if key in person:
+    print("Name is a valid dictionary key in the person object")
+else:
+    print("There is nothing like this!")
---
+++ not_operator.py
@@ -0,0 +1,11 @@
+# my_thing = False
+
+# if not my_thing: # ! is equal to "not" in python
+#     print("Print a statement in here")
+
+
+names = ["Ahmad", "Faraz", "Aayan"] 
+name = "Ali"
+
+if name not in names:
+    print(f"{name} is not a part of the Club!")
